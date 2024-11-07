89cafe79f8ccbac40aff9d3f7afbec92dc8d07fd
Marjiba Jamir <81945731+AdmiralAnne@users.noreply.github.com>
2024-11-07 11:10:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,9 @@ st.sidebar.write("""
 
 # Add sources with prominent display
 st.sidebar.info(
-    "🔗 Explore more on job satisfaction and career alignment:\n"
-    "[How Americans View Their Jobs](https://www.pewresearch.org/social-trends/2023/03/30/how-americans-view-their-jobs/)\n\n"
-    "[More Insights on American Job Satisfaction](https://www.pewresearch.org/social-trends/2023/03/30/how-americans-view-their-jobs/)"
+    "🔗 LINKS:\n"
+    "\n [How Americans View Their Jobs](https://www.pewresearch.org/social-trends/2023/03/30/how-americans-view-their-jobs/)\n\n"
+    "\n [More Insights on American Job Satisfaction](https://www.pewresearch.org/social-trends/2023/03/30/how-americans-view-their-jobs/)"
 )
 
 # sidebar
